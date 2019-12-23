282f938b2cfe6a3dd9b55787660799eb950a9333
tuongle2604 <tuongle2604@gmail.com>
2019-12-23 22:25:42 +0700

    add repnsive

--- src/components/AppHeader/index.vue
+++ src/components/AppHeader/index.vue
@@ -1,38 +1,44 @@
 <template>
-  <div class="container header">
+  <div class="container-md header">
     <router-link class="header__brand" :to="{ path: '/' }">
       <img src="@/assets/logo.png" class="header__brand-image" />
-      <p class="header__brand-name">Realworld</p>
+      <span class="header__brand-name">Realworld</span>
     </router-link>
-    <ul class="header__navbar">
+
+    <!-- dropdown menu icon in mobile -->
+    <span class="header__dropdown-icon-wrapper" @click="isExpand = !isExpand">
+      <font-awesome-icon icon="bars" class="header__dropdown-icon-bar"/>
+    </span>
+
+    <ul class="header__navbar" :class="{'expand': isExpand}">
       <li class="header__navbar-item">
-        <router-link to="/home" class="header__navbar-item__link">
+        <router-link to="/home" class="header__navbar-item__link" @click.native="colapseMenu">
           Home
         </router-link>
       </li>
       <li class="header__navbar-item">
-        <router-link to="/article" class="header__navbar-item__link">
+        <router-link to="/article" class="header__navbar-item__link" @click.native="colapseMenu">
           New Article
         </router-link>
       </li>
       <li class="header__navbar-item">
-        <router-link to="/settings" class="header__navbar-item__link">
+        <router-link to="/settings" class="header__navbar-item__link" @click.native="colapseMenu">
           <font-awesome-icon icon="cog" class="header__navbar-item__icon" />
           Settings
         </router-link>
       </li>
       <li class="header__navbar-item">
-        <router-link to="/profile/tuongle" class="header__navbar-item__link">
+        <router-link to="/profile/tuongle" class="header__navbar-item__link" @click.native="colapseMenu">
           Tuong Le
         </router-link>
       </li>
       <li class="header__navbar-item">
-        <router-link to="/login" class="header__navbar-item__link">
+        <router-link to="/login" class="header__navbar-item__link" @click.native="colapseMenu">
           Signin
         </router-link>
       </li>
       <li class="header__navbar-item">
-        <router-link to="/register" class="header__navbar-item__link">
+        <router-link to="/register" class="header__navbar-item__link" @click.native="colapseMenu">
           Signup
         </router-link>
       </li>
@@ -41,7 +47,18 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isExpand: false
+    }
+  },
+  methods: {
+    colapseMenu() {
+      this.isExpand = false;
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
