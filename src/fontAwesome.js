29bd926861efedac2f19801d2852917741a2c4e3
tuongle2604 <tuongle2604@gmail.com>
2020-11-01 00:41:10 +0700

    import only used icon

--- src/fontAwesome.js
+++ src/fontAwesome.js
@@ -1,6 +1,24 @@
 import Vue from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-library.add(fas);
+import {
+  faBars,
+  faHeart,
+  faEdit,
+  faTrash,
+  faCog,
+  faPlus,
+  faUserEdit,
+  faPencilAlt
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faBars);
+library.add(faHeart);
+library.add(faEdit);
+library.add(faTrash);
+library.add(faCog);
+library.add(faPlus);
+library.add(faUserEdit);
+library.add(faPencilAlt);
+
 Vue.component("font-awesome-icon", FontAwesomeIcon);
