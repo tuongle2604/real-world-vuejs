19354c67fba6eb7375c0b45640222013786cb25b
tuongle2604 <tuongle2604@gmail.com>
2020-11-01 00:42:04 +0700

    remove lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,13 @@ import VueRouter from "vue-router";
 import store from "@/store";
 import TokenService from "@/helper/tokenService";
 import HomePage from "@/pages/HomePage";
+import LoginPage from "@/pages/LoginPage";
+import RegisterPage from "@/pages/RegisterPage";
+import ArticleCreatePage from "@/pages/ArticleCreatePage";
+import ArticleEditPage from "@/pages/ArticleCreatePage";
+import ArticlePage from "@/pages/ArticlePage";
+import SettingsPage from "@/pages/SettingsPage";
+import ProfilePage from "@/pages/ProfilePage";
 
 Vue.use(VueRouter);
 
@@ -15,41 +22,41 @@ const routes = [
   {
     path: "/login",
     name: "LoginPage",
-    component: () => import("../pages/LoginPage")
+    component: LoginPage
   },
   {
     path: "/register",
     name: "RegisterPage",
-    component: () => import("../pages/RegisterPage")
+    component: RegisterPage
   },
   {
     path: "/article-create",
     name: "ArticleCreatePage",
-    component: () => import("../pages/ArticleCreatePage"),
+    component: ArticleCreatePage,
     meta: { requiresAuth: true }
   },
   {
     path: "/article-edit/:slug",
     name: "ArticleEditPage",
-    component: () => import("../pages/ArticleCreatePage"),
+    component: ArticleEditPage,
     props: true,
     meta: { requiresAuth: true }
   },
   {
     path: "/article/:slug",
     name: "ArticlePage",
-    component: () => import("../pages/ArticlePage")
+    component: ArticlePage
   },
   {
     path: "/settings",
     name: "SettingsPage",
-    component: () => import("../pages/SettingsPage"),
+    component: SettingsPage,
     meta: { requiresAuth: true }
   },
   {
     path: "/profile/:username",
     name: "ProfilePage",
-    component: () => import("../pages/ProfilePage")
+    component: ProfilePage
   }
 ];
 
